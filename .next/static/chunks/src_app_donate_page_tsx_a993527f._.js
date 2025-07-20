28aa030d583c89757e4a605b47fc6a37a5a043dc
João Pedro Guizelini <113556742+jpguizelini@users.noreply.github.com>
2025-07-20 18:37:56 -0300

    Add .next/static/chunks/src_app_donate_page_tsx_a993527f._.js 1753047496506

---
+++ .next/static/chunks/src_app_donate_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_d688f2c3._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_27804559._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_906fdbc6._.js",
+  "static/chunks/node_modules_zod_7a42b4f2._.js",
+  "static/chunks/node_modules_f9f3573b._.js"
+],
+    source: "dynamic"
+});

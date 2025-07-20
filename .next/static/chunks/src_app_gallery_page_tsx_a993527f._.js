e2860c446f35d12d6d395daecde276d20b2c739d
João Pedro Guizelini <113556742+jpguizelini@users.noreply.github.com>
2025-07-20 18:38:02 -0300

    Add .next/static/chunks/src_app_gallery_page_tsx_a993527f._.js 1753047501932

---
+++ .next/static/chunks/src_app_gallery_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_dadb1f1e._.js",
+  "static/chunks/src_components_a28da762._.js"
+],
+    source: "dynamic"
+});
